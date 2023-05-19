07334be95423182b100cb5a453bcb4e0753583a2
calum <calum.quinn@heig-vd.ch>
2023-05-19 12:51:54 +0200

    Test 5 passe pas je ne sais pas encore pourquoi et il manque les throws

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -73,29 +73,5 @@ int evalue(string const& expression ) {
       throw bad_expression();
    }
 
-
    return temp;
-
-//   Dijkstra
-//
-//   fonction évaluer(expression):
-//   pileOp ← Pile vide
-//   pileVal ← Pile vide
-//   tant qu’il reste des éléments à lire
-//   e ← lire un élément(expression)
-//   si e est '('
-//   ignorer
-//   sinon, si e est un opérateur
-//   pileOp.push(e)
-//   sinon, si e est une valeur numérique
-//   pileVal.push(e)
-//   sinon, si e est ')'
-//   v2 ← pileVal.pop()
-//   v1 ← pileVal.pop()
-//   op ← pileOp.pop()
-//   r ← évaluer v1 op v2
-//   pileVal.push(r)
-//   retourner pileVal.pop()
-
-
 }
--- src/evaluator_recursive.cpp
+++ src/evaluator_recursive.cpp
@@ -1,5 +1,7 @@
 #include "evaluator.h"
 #include <string>
+#include <algorithm>
+#include <cmath>
 
 using opData = char;
 using valData = int;
@@ -23,6 +25,24 @@ valData effectuer (valData val1, opData op, valData val2) {
    }
 }
 
+int chiffresDansNbr(int nbr) {
+   return int(log10(nbr)) + 1;
+}
+
+int valeurLongue(string expression, size_t& pos) {
+   size_t j = 0;
+   while(isdigit(expression[pos + j])){
+      ++j;
+   }
+
+   int valeur = stoi(expression.substr(pos,j));
+
+   //Avance dans l'expression pour ne pas contrôler les autre chiffres du même nombre
+   pos += chiffresDansNbr(valeur) - 1;
+
+   return valeur;
+}
+
 int evalueRecursive(string expression) {
    for (size_t i = 0; i < expression.size(); ++i) {
       int val1;
@@ -30,34 +50,38 @@ int evalueRecursive(string expression) {
       char operateur;
       if (expression[i] == '(') {
          int compteur = 0;
-         size_t j = 0;
+         size_t j = i;
          while (compteur != -1) {
+            ++j;
             if (expression[j] == '(') {
                ++compteur;
             } else if (expression[j] == ')') {
                --compteur;
             }
-            ++j;
          }
-         expression.replace(i, j, evalueRecursive(expression.substr(i, j)));
+         //On remplace les parenthèses par leur contenu
+         expression.replace(i, j + 1, to_string(evalueRecursive(expression.substr(i + 1, j - i - 1))));
+         //On recommence au début pour trouver val1
+         i = -1;
       }
       else if (isdigit(expression[i])) {
-         size_t j = 1;
-         while(isdigit(expression[i + j])){
-            ++j;
-         }
-
-         val1 = stoi(expression.substr((i,j)));
-         val2 = evalueRecursive(LaPartieQuiSuit selon le type de caractère qui suit l'opérateur');
+         val1 = valeurLongue(expression,i);
       }
       else if (estOperateur(expression[i])) {
          operateur = expression[i];
+         char apresOp = expression[i + 1];
+         if (isdigit(apresOp)) {
+            val2 = valeurLongue(expression,++i);
+            expression.replace(0, chiffresDansNbr(val1) + 1 + chiffresDansNbr(val2), to_string(effectuer(val1,operateur,val2)));
+         }
       }
    }
+   return stoi(expression);
 }
 
 int evalue(string const& expression ) {
    string exprRecursive = expression;
-   return stoi(evalueRecursive(expression));
+   //Retire les espaces vide
+   exprRecursive.erase(remove_if(exprRecursive.begin(), exprRecursive.end(), (int(*)(int))(isspace)), exprRecursive.end());
+   return evalueRecursive(exprRecursive);
 }
-
